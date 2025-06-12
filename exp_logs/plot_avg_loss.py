6add23c80734589019d140850b07294ffdccf3b2
Pratik Dash <pratik@wosler.ca>
2025-06-12 13:49:16 +0000

    added log file for ViT_small run

--- exp_logs/plot_avg_loss.py
+++ exp_logs/plot_avg_loss.py
@@ -11,7 +11,8 @@ import os
 # log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vit_small_dataset_new_ep20')
 # log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vit_tiny_with_val_ep5')
 # log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vit_tiny_with_val_ep14')
-log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vit_tiny_with_val_ep100')
+# log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vit_tiny_with_val_ep100')
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vit_small_with_val_ep100')
 
 csv_files = glob.glob(os.path.join(log_dir, '*.csv'))
 
